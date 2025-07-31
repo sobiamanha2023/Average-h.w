6e213145538f5feca99b49806b240981f8263fe4
sobiamanha2023 <sobiamanha1@gmail.com>
2025-07-31 11:37:29 +0800

    Add files via upload

---
+++ hw avg.py
@@ -0,0 +1,22 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+e = int(input("Enter fifth number: "))
+f = int(input("Enter sixth number: "))
+g = int(input("Enter seventh number: "))
+h = int(input("Enter eighth number: "))
+i = int(input("Enter nineth number: "))
+j = int(input("Enter tenth number: "))
+
+total = a + b + c + d + e + f + g + h + i + j
+
+avg = total/10
+
+print("Result: ",avg)
+
+m = max(a,b,c,d,e,f,g,h,i,j)
+print("Maximum number:",m)
+
+n = min(a,b,c,d,e,f,g,h,i,j)
+print("Minimum number:",n)
